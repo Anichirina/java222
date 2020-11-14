e60575b27125820c28f17ea6f06d2e8b9304451d
Irina Anichkova <anichkova2016@yandex.ru>
2020-11-14 21:03:52 +0300

    new file

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+    public float calculate(float weight, float growth) {
+        float index = weight / growth / growth * 100*100;
+        return index;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        float weight = 63F;
+        float growth = 165F;
+        float index = service.calculate( weight, growth);
+                System.out.println(index);
+    }
+}
